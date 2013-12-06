0a3b297db4796f0453fb40cab82a744405a13aa9
mackief <davidepucci21@gmail.com>
2013-12-06 22:49:46 +0100

    Fixed non-existing generic-device error

--- target/product/full.mk
+++ target/product/full.mk
@@ -20,7 +20,6 @@
 # entirely appropriate to inherit from for on-device configurations.
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/board/generic/device.mk)
 
 # Overrides
 PRODUCT_NAME := full
